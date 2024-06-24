e891320f33dca56041a0df334c09c9cab74f0962
Guido Pérez Moro <90873217+GuidoPerezMoro@users.noreply.github.com>
2024-06-24 01:38:47 -0300

    Optimizacion de imports

--- src/main/java/com/entidades/buenSabor/BuenSaborApplication.java
+++ src/main/java/com/entidades/buenSabor/BuenSaborApplication.java
@@ -1,20 +1,9 @@
 package com.entidades.buenSabor;
 
-import com.entidades.buenSabor.domain.entities.*;
-import com.entidades.buenSabor.domain.enums.*;
-import com.entidades.buenSabor.repositories.*;
-import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 
 @SpringBootApplication
